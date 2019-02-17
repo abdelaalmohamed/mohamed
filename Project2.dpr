c13436b81d28e3e238d4359aa028d6b8d1c3e798
abdelaalmohamed <47691190+abdelaalmohamed@users.noreply.github.com>
2019-02-17 18:53:48 +0300

    первый лаб

---
+++ Project2.dpr
@@ -0,0 +1,16 @@
+program Project2;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils,
+  Unit1 in '..\Unit1.pas' {Form1};
+
+begin
+  Writeln('Hello, World from Delphi!');
+  Writeln('This is the console application');
+  Writeln;
+  Writeln('Press <Enter> to quit.');
+  Readln; 
+
+end.
