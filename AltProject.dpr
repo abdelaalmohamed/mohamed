a343d45277448bf9278c9df825e5332815cba1a1
abdelaalmohamed <47691190+abdelaalmohamed@users.noreply.github.com>
2019-02-20 14:18:24 +0300

    Lab1

---
+++ AltProject.dpr
@@ -0,0 +1,17 @@
+program AltProject;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils;
+
+begin
+  { TODO -oUser -cConsole Main : Insert code here }
+    Writeln('Hello, World from Delphi!');
+    Writeln('This is the console application');
+    Writeln;
+    Writeln('Press <Enter> to quit.');
+    Readln;
+
+end.
+ 
